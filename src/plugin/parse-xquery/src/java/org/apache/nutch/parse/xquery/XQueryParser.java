d1334b53b3e753d0bc37160d60d442c32c14be11
Niklas <niklas@niklas-UX31E.(none)>
2012-02-18 17:03:16 +0100

    Added XPath as an optional rule to select an XQuery.

--- src/plugin/parse-xquery/src/java/org/apache/nutch/parse/xquery/XQueryParser.java
+++ src/plugin/parse-xquery/src/java/org/apache/nutch/parse/xquery/XQueryParser.java
@@ -30,7 +30,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
+import java.util.Iterator;
 import javax.xml.namespace.QName;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -41,6 +41,10 @@ import javax.xml.xquery.XQException;
 import javax.xml.xquery.XQPreparedExpression;
 import javax.xml.xquery.XQSequence;
 import javax.xml.xquery.XQStaticContext;
+
+import javax.xml.xpath.*;
+import javax.xml.namespace.NamespaceContext;
+
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
@@ -78,13 +82,15 @@ public class XQueryParser implements HtmlParseFilter {
 	private final static Log LOG = LogFactory.getLog(XQueryParser.class);
 	private XQConnection xqConnection = null;
 	private Configuration conf;
-	private Map<String,List<PatternQueryPair>> rules = new HashMap<String, List<PatternQueryPair>>();
+	private Map<String,List<XQueryIdentifier>> rules = new HashMap<String, List<XQueryIdentifier>>();
 	
-	private class PatternQueryPair {
+    private class XQueryIdentifier {
 		public Pattern pattern;
+	        public XPathExpression xpath;
 		public XQPreparedExpression expr;
-		public PatternQueryPair(Pattern pattern, XQPreparedExpression expr) {
+	        public XQueryIdentifier(Pattern pattern, XPathExpression xpath, XQPreparedExpression expr) {
 			this.pattern = pattern;
+			this.xpath = xpath;
 			this.expr = expr;
 		}		
 	}
@@ -142,9 +148,13 @@ public class XQueryParser implements HtmlParseFilter {
 	}
 
 	public String parse(DocumentFragment doc, String urlStr, String baseUrlStr)
-			throws MalformedURLException, XQException {
+	    throws MalformedURLException, XQException,XPathExpressionException {
 		String parseOutput = null;
-		XQPreparedExpression expr = this.matchURL(urlStr);
+		XQPreparedExpression expr=null;// = this.matchURL(urlStr);
+		LinkedList<XQueryIdentifier> potentialExpressions = this.matchURL(urlStr);
+		if( potentialExpressions != null)
+		    expr = this.matchXPath(potentialExpressions, doc);
+
 		if (expr != null) {
 			expr.bindNode(XQConstants.CONTEXT_ITEM, doc, null);
 			QName[] externalVariables = expr.getAllExternalVariables();
@@ -160,26 +170,49 @@ public class XQueryParser implements HtmlParseFilter {
 		return parseOutput;
 	}
 
-	public XQPreparedExpression matchURL(String urlStr) throws MalformedURLException {
+	public LinkedList<XQueryIdentifier> matchURL(String urlStr) throws MalformedURLException {
 		URL url = new URL(urlStr);
 		String domain = url.getHost();
 		String pathAndQuery = url.getPath() + (url.getQuery() != null ? "?" + url.getQuery() : "");
 		
-		LinkedList<PatternQueryPair> ruleList = (LinkedList<PatternQueryPair>) this.rules.get(domain);
+		LinkedList<XQueryIdentifier> res = new LinkedList<XQueryIdentifier>();
+		
+		LinkedList<XQueryIdentifier> ruleList = (LinkedList<XQueryIdentifier>) this.rules.get(domain);
 		if (null != ruleList) {
-			for (PatternQueryPair pair: ruleList) {
+			for (XQueryIdentifier pair: ruleList) {
 				Matcher matcher = pair.pattern.matcher(pathAndQuery);
+				
 				if (matcher.matches())
-					return pair.expr;
+				    res.add(pair);
+				//return pair.expr;
 			}
 		}
-		return null;
+		if(res.size()== 0){
+		    return null;
+		}		
+		return res;
+	}
+
+        public XQPreparedExpression matchXPath(LinkedList<XQueryIdentifier> potentialExpressions,DocumentFragment doc) throws XPathExpressionException{
+	    XQPreparedExpression ret = null;
+	    for(XQueryIdentifier pair : potentialExpressions){
+		if(pair.xpath != null){
+		    String res =  (String)pair.xpath.evaluate(doc,XPathConstants.STRING);
+		    if(res != null && !res.equals("")){ // if result is non-empty, we have a match.
+			ret = pair.expr;
+			break;
+		    }
+		}else{
+		    ret = pair.expr; // Use default (No xpath) if no other match is found.
+		}
+	    }
+	    return ret;
 	}
 
 	public void printParseRules() {
-		for (Entry<String, List<PatternQueryPair>> domain : this.rules.entrySet()) {
+		for (Entry<String, List<XQueryIdentifier>> domain : this.rules.entrySet()) {
 			System.out.println(domain.getKey());
-			for (PatternQueryPair pair: domain.getValue()) {
+			for (XQueryIdentifier pair: domain.getValue()) {
 				System.out.println("\t" + pair.pattern);
 			}
 		}
@@ -209,19 +242,47 @@ public class XQueryParser implements HtmlParseFilter {
 			}
 			String domain = rule.getAttribute("domain");
 			String patternStr = rule.getAttribute("pattern");
+			String xpathString = rule.getAttribute("xpath");
+			XPathExpression xp=null;
+			if(xpathString != null && !xpathString.equals("")){
+			    XPath xpath =  XPathFactory.newInstance().newXPath();
+			    // set namespace to http://www.w3.org/1999/xhtml
+			    xpath.setNamespaceContext(new SimpleNamespaceContext());
+			    xp = xpath.compile(xpathString);
+			}
+
 			String xquery = rule.getAttribute("xquery");
 			if (!this.rules.containsKey(domain))
-				this.rules.put(domain, new LinkedList<PatternQueryPair>());
+				this.rules.put(domain, new LinkedList<XQueryIdentifier>());
 			URL resolvedXQueryPath = rulesResource.toURI().resolve(xquery).toURL();
 			InputStream xqis = resolvedXQueryPath.openStream();
 			XQPreparedExpression expr = this.xqConnection.prepareExpression(xqis);
 			Pattern pattern = Pattern.compile(patternStr);
-			this.rules.get(domain).add(new PatternQueryPair(pattern, expr));
+			this.rules.get(domain).add(new XQueryIdentifier(pattern, xp, expr));
 
 			if (LOG.isDebugEnabled()) { LOG.debug("Parse rule: " + domain + " " + patternStr + " " + xquery); }
 		}
 	}
 	
+    private class SimpleNamespaceContext implements NamespaceContext{
+        public String getNamespaceURI(String prefix){
+	    if(prefix != null && prefix.equals("h")){
+		return "http://www.w3.org/1999/xhtml";
+	    }else{		    
+		return null;
+	    }
+        }
+        
+        public String getPrefix(String namespace){
+	    return null;
+	}
+
+        public Iterator getPrefixes(String namespace){
+            return null;
+        }
+    }  
+  
+    
 	private Document readXMLDocument(InputStream is) throws Exception {
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		factory.setNamespaceAware(true);
